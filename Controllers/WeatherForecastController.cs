f19adf7ef2274a1e6d81d6efde618f3abd28d1c1
Atiq Chowdhury <achowdhury@sfamc.com.au>
2021-07-24 23:58:42 +1000

    Added Repository and UserController

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Insurance.Controllers
+namespace AngularDemo.Controllers
 ***REMOVED***
     [ApiController]
     [Route("[controller]")]
---
+++ Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Insurance.Repository
+***REMOVED***
+    public interface IRepository
+    ***REMOVED***
+        Task<List<T>> SelectAll<T>() where T : class;
+        Task CreateAsync<T>(T entity) where T : class;
+        
+***REMOVED***
+***REMOVED***
