a7e5c76d6c629ae5ceac17943387c469bd2dee84
Atiq Chowdhury <achowdhury@sfamc.com.au>
2021-07-25 03:42:19 +1000

    Added user form

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,8 +8,34 @@ import ***REMOVED*** FormsModule ***REMOVED*** from '@angular/forms';
 import ***REMOVED*** HttpClientModule ***REMOVED*** from '@angular/common/http';
 import ***REMOVED*** BrowserAnimationsModule ***REMOVED*** from '@angular/platform-browser/animations';
 import ***REMOVED***
-  MatDatepickerModule, MatNativeDateModule, MatFormFieldModule
+  MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MAT_DATE_FORMATS, NativeDateAdapter, DateAdapter, MatDateFormats
 ***REMOVED*** from '@angular/material';
+
+const MY_DATE_FORMATS = ***REMOVED***
+  parse: ***REMOVED***
+    dateInput: ***REMOVED*** day: 'numeric', month: 'numeric', year: 'numeric' ***REMOVED***
+***REMOVED***
+  display: ***REMOVED***
+    dateInput: 'input',
+    monthYearLabel: ***REMOVED*** year: 'numeric', month: 'short' ***REMOVED***,
+    dateA11yLabel: ***REMOVED*** year: 'numeric', month: 'long', day: 'numeric' ***REMOVED***,
+    monthYearA11yLabel: ***REMOVED*** year: 'numeric', month: 'long' ***REMOVED***,
+***REMOVED***
+***REMOVED***;
+
+export class AppDateAdapter extends NativeDateAdapter ***REMOVED***
+
+  format(date: Date, displayFormat: Object): string ***REMOVED***
+    if (displayFormat === 'input') ***REMOVED***
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `$***REMOVED***day***REMOVED***/$***REMOVED***month***REMOVED***/$***REMOVED***year***REMOVED***`;
+***REMOVED*** else ***REMOVED***
+      return date.toDateString();
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
 @NgModule(***REMOVED***
   declarations: [
@@ -28,7 +54,8 @@ import ***REMOVED***
     MatNativeDateModule,
     MatFormFieldModule
   ],
-  providers: [],
+  providers: [***REMOVED*** provide: DateAdapter, useClass: AppDateAdapter ***REMOVED***,
+    ***REMOVED*** provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS ***REMOVED***],
   bootstrap: [AppComponent]
 ***REMOVED***)
 export class AppModule ***REMOVED*** ***REMOVED***
--- ClientApp/src/app/users/user-form/user-form.component.ts
+++ ClientApp/src/app/users/user-form/user-form.component.ts
@@ -1,8 +1,11 @@
-import ***REMOVED*** Component, OnInit ***REMOVED*** from '@angular/core';
+import ***REMOVED*** Component, EventEmitter, OnInit, Output ***REMOVED*** from '@angular/core';
 import ***REMOVED*** UsersService ***REMOVED*** from '../../shared/users.service';
 import ***REMOVED*** NgForm ***REMOVED*** from '@angular/forms';
 import ***REMOVED*** Users ***REMOVED*** from '../../shared/users.model';
- 
+import ***REMOVED*** MatDatepickerInputEvent ***REMOVED*** from '@angular/material';
+ import * as _moment from 'moment';
+import ***REMOVED*** Moment ***REMOVED*** from 'moment';
+import * as moment from 'moment';
 @Component(***REMOVED***
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
@@ -10,7 +13,8 @@ import ***REMOVED*** Users ***REMOVED*** from '../../shared/users.model';
   ]
 ***REMOVED***)
 export class UserFormComponent implements OnInit ***REMOVED***
- 
+
+  age:number;
   constructor(public service: UsersService) ***REMOVED***
      
 ***REMOVED***
@@ -18,7 +22,15 @@ export class UserFormComponent implements OnInit ***REMOVED***
   ngOnInit(): void ***REMOVED***
 ***REMOVED***
 
-   
+  handleDOBChange(event) ***REMOVED***
+    const m: Moment = event.value;
+    
+    const userDOB = moment('1989/11/17', 'YYYY/M/D');
+
+    this.age = moment().diff(userDOB, 'years')
+
+    console.log(this.age)
+***REMOVED***
   onSubmit(form: NgForm) ***REMOVED***
     if(this.service.formData.id == 0) //we will use the id as identifier for updating or insertion
     this.insertRecord(form);
