605e8691ea49f2c2e456d35f2c19e8ffdae4ec39
Atiq Chowdhury <achowdhury@sfamc.com.au>
2021-07-25 06:35:37 +1000

    created Occupation api, model and repository layer.  Called a web service to populate the occupation dropdown.  Fixed UI layout

--- ClientApp/src/app/users/user-form/user-form.component.ts
+++ ClientApp/src/app/users/user-form/user-form.component.ts
@@ -6,6 +6,10 @@ import { MatDatepickerInputEvent } from '@angular/material';
  import * as _moment from 'moment';
 import { Moment } from 'moment';
 import * as moment from 'moment';
+import { BehaviorSubject } from 'rxjs';
+import { Occupation } from '../../shared/occupation.model';
+ 
+ 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
@@ -13,13 +17,21 @@ import * as moment from 'moment';
   ]
 })
 export class UserFormComponent implements OnInit {
-
-  age:number;
+  occupationValue: number;
+  age: number;
+  premium: number;
+  firstname: string;
+  showResult: boolean;
+  occupations: Occupation[] = [];
   constructor(public service: UsersService) {
      
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  
+
+    this.service.getOccupation().subscribe(res => {
+      this.occupations = res;
+    });
   }
 
   handleDOBChange(event) {
@@ -29,15 +41,19 @@ export class UserFormComponent implements OnInit {
 
     this.age = moment().diff(userDOB, 'years')
 
-    console.log(this.age)
+    
   }
-  onSubmit(form: NgForm) {
-    if(this.service.formData.id == 0) //we will use the id as identifier for updating or insertion
-    this.insertRecord(form);
-    else
-    this.updateRecord(form);
+  onSubmit(form: NgForm) {    
+    const deathInsured = form.value.deathInsured;
+    if (deathInsured > 0 && this.occupationValue > 0 && this.age > 0) {
+      this.premium = (deathInsured * this.occupationValue * this.age) / 1000 * 12
+      this.showResult = true;
+    }
   }
-
+  setFactor(filterVal: any) {
+   
+    this.occupationValue = filterVal;
+  }
   insertRecord(form:NgForm) {
     this.service.postMember().subscribe(
       res => {
@@ -61,9 +77,9 @@ export class UserFormComponent implements OnInit {
       }
     );
   }
-
   resetForm(form: NgForm) {
-    form.form.reset();
-    this.service.formData = new Users();
+    form.setValue({ firstName: '', lastName: '', deathInsured: '' });
+    this.showResult = false;
   }
+  
 }
