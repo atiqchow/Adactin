7fda16b1daa9df33c52c2b5a2b6cf018fa598b78
Atiq Chowdhury <achowdhury@sfamc.com.au>
2021-07-24 23:57:40 +1000

    Add Model class

---
+++ Models/AppDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Insurance.Models
+***REMOVED***
+    public class AppDBContext:DbContext
+    ***REMOVED***
+        private readonly DbContextOptions _options;
+        public AppDBContext(DbContextOptions options) : base(options)
+        ***REMOVED***
+            _options = options;
+    ***REMOVED***
+
+        public DbSet<User> users ***REMOVED*** get; set; ***REMOVED***
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        ***REMOVED***
+            base.OnModelCreating(modelBuilder);
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Insurance.Models
+***REMOVED***
+    public class User
+    ***REMOVED***
+        [Key]
+        public long Id ***REMOVED*** get; set; ***REMOVED***        
+        public string FirstName ***REMOVED*** get; set; ***REMOVED***       
+        public string LastName ***REMOVED*** get; set; ***REMOVED*** 
+        public DateTime DOB ***REMOVED*** get; set; ***REMOVED***
+        public string Occupation ***REMOVED*** get; set; ***REMOVED***
+        public double DeathInsured ***REMOVED*** get; set; ***REMOVED***
+        public int Age ***REMOVED*** get; set; ***REMOVED***
+***REMOVED***
+***REMOVED***
