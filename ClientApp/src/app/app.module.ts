05117d06b58694165b8e17cafd0697c0a5d5909c
Atiq Chowdhury <achowdhury@sfamc.com.au>
2021-07-25 03:42:19 +1000

    Added user form

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,8 +8,34 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
-  MatDatepickerModule, MatNativeDateModule, MatFormFieldModule
+  MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MAT_DATE_FORMATS, NativeDateAdapter, DateAdapter, MatDateFormats
 } from '@angular/material';
+
+const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: { day: 'numeric', month: 'numeric', year: 'numeric' }
+  },
+  display: {
+    dateInput: 'input',
+    monthYearLabel: { year: 'numeric', month: 'short' },
+    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
+    monthYearA11yLabel: { year: 'numeric', month: 'long' },
+  }
+};
+
+export class AppDateAdapter extends NativeDateAdapter {
+
+  format(date: Date, displayFormat: Object): string {
+    if (displayFormat === 'input') {
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+    } else {
+      return date.toDateString();
+    }
+  }
+}
 
 @NgModule({
   declarations: [
@@ -28,7 +54,8 @@ import {
     MatNativeDateModule,
     MatFormFieldModule
   ],
-  providers: [],
+  providers: [{ provide: DateAdapter, useClass: AppDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
