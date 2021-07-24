e40a376a66c4a340c4cede8e9dab7ec67d6ef6cf
Atiq Chowdhury <achowdhury@sfamc.com.au>
2021-07-24 23:58:42 +1000

    Added Repository and UserController

---
+++ Repository/Repository.cs
@@ -0,0 +1,31 @@
+﻿using Insurance.Models;
+using Insurance.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Insurance.Repository
+***REMOVED***
+    public class Repository<TDbContext> : IRepository where TDbContext : DbContext
+    ***REMOVED***
+        protected TDbContext dbContext;
+
+        public Repository(TDbContext context)
+        ***REMOVED***
+            dbContext = context;
+    ***REMOVED***
+        public async Task CreateAsync<T>(T entity) where T : class
+        ***REMOVED***
+            this.dbContext.Set<T>().Add(entity);
+
+            _ = await this.dbContext.SaveChangesAsync();
+    ***REMOVED***
+        public async Task<List<T>> SelectAll<T>() where T : class
+        ***REMOVED***
+            return await this.dbContext.Set<T>().ToListAsync();
+    ***REMOVED***
+
+***REMOVED***
+***REMOVED***
