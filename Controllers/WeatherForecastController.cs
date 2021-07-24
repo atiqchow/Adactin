5f94df60e2a931da480fda9d54f34de97c3d57a7
Atiq Chowdhury <achowdhury@sfamc.com.au>
2021-07-24 23:58:42 +1000

    Added Repository and UserController

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Insurance.Controllers
+namespace AngularDemo.Controllers
 {
     [ApiController]
     [Route("[controller]")]
